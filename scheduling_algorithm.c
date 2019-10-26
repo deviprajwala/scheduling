54aa4d3c3ebe0194036590aa8b3ddb27ba24add6
deviprajwala <deviprajwala3@gmail.com>
2019-10-26 10:34:02 +0530

    commit

--- scheduling_algorithm.c
+++ scheduling_algorithm.c
@@ -5,19 +5,23 @@ struct process
 {
  int time;
  char name[10];
+ int priority;
  struct process *link;
- struct process *l_link;
 };
 typedef struct process *process;
 
 process head;
+int a[10];
+int p[10];
 
 process get_process();
 void get_pro(process head,int n);
 void display_list(process head);
 void display(process head);
 void first_come_first_serve(process head);
-void shortest_job_first(process head);
+void shortest_job_first(process head,int n);
+void copy(process head,int n);
+void priority(process head,int n);
 
 
 process get_process()
@@ -25,13 +29,12 @@ process get_process()
  process x;
  x=(process)malloc(sizeof(struct process));
  x->link=NULL;
- x->l_link=NULL;
  return x;
 }
 
 void get_pro(process head,int n)
 {
- int i,t;
+ int i,t,p;
  char str[10];
  process temp,cur,prev;
 // printf("Enter the process name and it's time\n");
@@ -44,10 +47,13 @@ void get_pro(process head,int n)
    printf("Time:");
    scanf("%d",&t);
    temp->time=t;
+   printf("Priority:");
+   scanf("%d",&p);
+   temp->priority=p;
    if(head->link==NULL)
    {
       head->link=temp;
-      temp->l_link=head;
+      
    }
    else
    {
@@ -59,7 +65,6 @@ void get_pro(process head,int n)
        cur=cur->link;
      }
      prev->link=temp;
-     temp->l_link=head;
    }
  }
 }
@@ -71,11 +76,12 @@ void display_list(process head)
  printf("The list of processes and thier time is\n");
  printf("\n__________________________________________\n");
  cur=head->link;
- printf("NAME    TIME\n");
+ printf("NAME    TIME   PRIORITY\n");
  while(cur!=NULL)
  {
    printf("%s      ",cur->name);
    printf("%d      ",cur->time);
+   printf("%d      ",cur->priority);
    printf("\n");
    cur=cur->link;
  }
@@ -106,30 +112,110 @@ void first_come_first_serve(process head)
  display(head);
 }
 
-void shortest_job_first(process head)
+void shortest_job_first(process head,int  n)
 {
- process cur,prev,next;
- prev=head;
- cur=prev->link;
- next=cur->link;
- while(next!=NULL)
+ process cur;
+ cur=head->link;
+ int temp,i,j,key,k;
+ for(i=1;i<=n;i++)
  {
- while(cur!=NULL)
+  for(j=i+1;j<=n;j++)
+  {
+  if(a[i]>a[j])
+  {
+   temp=a[i];
+   a[i]=a[j];
+   a[j]=temp;
+  }
+  }
+ }
+ for(i=1;i<=n;i++)
  {
-   if(cur->time>next->time)
-   {
-    prev->link=next;
-    next->link=prev;
-   }
-  cur=cur->link;
-  next=cur->link;
+  printf("%d ",a[i]);
  }
- prev=prev->link;
- cur=prev->link;
- next=cur->link;
+ printf("\n");
+ for(i=1;i<=n;i++)
+ {
+  key=a[i];
+  while(cur!=NULL)
+  {
+    if(cur->time==key)
+    {
+      printf("%s",cur->name);
+      for(k=1;k<=key;k++)
+      {
+        printf("-");
+      }
+      printf(" ");
+    }
+   cur=cur->link;
+  }
+  cur=head->link;
  }
+ printf("\n");
 }
 
+void copy(process head,int n)
+{
+  process cur;
+  cur=head->link;
+  int i;
+  for(i=1;i<=n;i++)
+  {
+   a[i]=cur->time;
+   p[i]=cur->priority;
+   cur=cur->link;
+  // strcpy(b[i],cur->name);
+  } 
+  for(i=1;i<=n;i++)
+  {
+   printf("%d ",a[i]);
+  }
+  printf("\n");
+}
+
+void priority(process head,int n)
+{
+ process cur;
+ cur=head->link;
+ int temp,i,j,key,k;
+ for(i=1;i<=n;i++)
+ {
+  for(j=i+1;j<=n;j++)
+  {
+  if(p[i]>p[j])
+  {
+   temp=p[i];
+   p[i]=p[j];
+   p[j]=temp;
+  }
+  }
+ }
+ for(i=1;i<=n;i++)
+ {
+  printf("%d ",p[i]);
+ }
+ printf("\n");
+ for(i=1;i<=n;i++)
+ {
+  key=p[i];
+  while(cur!=NULL)
+  {
+    if(cur->priority==key)
+    {
+      printf("%s",cur->name);
+      for(k=1;k<=cur->time;k++)
+      {
+        printf("-");
+      }
+      printf(" ");
+    }
+   cur=cur->link;
+  }
+  cur=head->link;
+ }
+ printf("\n");
+}
 
 int main()
 {
@@ -142,8 +228,11 @@ int main()
  //display(head);
  printf("\nFIRST COME FIRST SERVE\n");
  first_come_first_serve(head);
+ copy(head,n);
  printf("\nSHORTEST JOB FIRST\n");
- shortest_job_first(head);
- display(head);
+ shortest_job_first(head,n);
+ printf("\nPRIORITY JOB SCHEDULING\n");
+ priority(head,n);
+ //display(head);
  return 0;
 }
